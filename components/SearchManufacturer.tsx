38cd72810193876b3a5962ef59a61642385909f4
samandar <samandargofurov236@gmail.com>
2024-10-15 16:29:09 +0500

    first commit

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,101 @@
+"use client"
+
+import { Fragment, useState } from "react";
+import Image from "next/image";
+import {
+  Combobox,
+  ComboboxButton,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+  Transition,
+} from "@headlessui/react";
+import { Scrollbars } from 'react-custom-scrollbars-2';
+
+import { manufacturers } from "@/constants";
+import { SearchManufacturerProps } from "@/types";
+
+const SearchManufacturer = ({
+  manufacturer,
+  setManufacturer,
+}: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
+
+  const filteredManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
+
+  return (
+    <>
+      <div className="search-manufacturer group overflow-auto">
+        <Combobox value={manufacturer} onChange={setManufacturer}>
+          <div className="relative w-full">
+            <ComboboxButton className="absolute top-[14px]">
+              <Image
+                src="/car-logo.svg"
+                width={20}
+                height={20}
+                className="ml-4"
+                alt="Car Logo"
+              />
+            </ComboboxButton>
+
+            <ComboboxInput
+              className="search-manufacturer__input"
+              placeholder="Volkswagen"
+              displayValue={(manufacturer: string) => manufacturer}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+
+            <Transition
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+              afterLeave={() => setQuery("")}
+            >
+              <ComboboxOptions>
+                <Scrollbars
+                  style={{ width: "100%", height: "150px" }}
+                  autoHide
+                  autoHideTimeout={1000}
+                  autoHideDuration={200}
+                >
+                  {filteredManufacturers.map((item) => (
+                    <ComboboxOption
+                      key={item}
+                      className={({ active }) => `relative search-manufacturer__option ${
+                        active ? "bg-primary-blue text-white" : "text-gray-900"
+                      }`}
+                      value={item}
+                    >
+                      {({ selected, active }) => (
+                        <>
+                          <span
+                            className={`block truncate ${
+                              selected ? "font-medium" : "font-normal"
+                            }`}
+                          >
+                            {item}
+                          </span>
+                        </>
+                      )}
+                    </ComboboxOption>
+                  ))}
+                </Scrollbars>
+              </ComboboxOptions>
+            </Transition>
+          </div>
+        </Combobox>
+      </div>
+    </>
+  );
+};
+
+export default SearchManufacturer;
---
+++ utils/index.ts
@@ -0,0 +1,27 @@
+export async function fetchCars() {
+     const headers = {
+          'X-RapidAPI-KEY': 'ab4041aba8msh19560050a4b84fcp111c02jsn4389eb87639f',
+          'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+     }
+
+     const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+          headers: headers,
+     });
+
+     const result = await response.json();
+
+     return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+     const basePricePerDay = 50;
+     const milleageFactor = 0.1;
+     const ageFactor = 0.05;
+
+     const milleageRate = city_mpg * milleageFactor;
+     const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+     const rentalRatePerDay = basePricePerDay + milleageRate + ageRate;
+
+     return rentalRatePerDay.toFixed(0);
+};
